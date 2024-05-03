c9d37da8499975f55ba344308d626317a241feac
Akbarsab07 <akbarsab1998@gmail.com>
2024-05-03 20:36:25 +0530

    Test Push

---
+++ Test.java
@@ -0,0 +1,100 @@
+public class Test{
+    public static void main(String[] args) {
+
+        //Examples of Byte Datatype
+        byte asciiValue = 97;
+        byte minValue = -128;
+        byte zeroValue = 0;
+        byte maxValue = 127;
+        byte assciValueofA = 97; 
+        byte octalValue = 012; 
+        byte hexadecimalValue = 0xA; 
+        byte calculatedValue = (byte) (10 + 20); 
+        byte literalValue = 30;
+        byte Karnatakacities = 32;        
+
+        //Examples of Short Datatype
+        short numberOfStudents = 20;
+        short maxTemperature = 100;
+        short employeeID = 1234;
+        short productCode = 5678;
+        short distanceInMeters = 5000;
+        short roomNumber = 102;
+        short yearOfBirth = 1995;
+        short quantityInStock = 150;
+        short voltageLevel = 220;
+        short examScore = 85;
+
+        //Examples of Int Datatype
+        int numberOfStudentsi = 100;
+        int maxTemperaturei = 40;
+        int employeeIDi = 123456;
+        int productCodei = 789012;
+        int distanceInMetersi = 5000;
+        int roomNumberi = 101;
+        int yearOfBirthi = 1990;
+        int quantityInStocki = 500;
+        int voltageLeveli = 220;
+        int examScorei = 90;
+
+        //Examples of Long Datatype
+        long populationOfCity = 1000000L;
+        long distanceToMoon = 384400000L;
+        long nationalDebt = 235000000000L;
+        long numberOfStars = 10000000000L;
+        long ageOfUniverse = 13798000000L;
+        long earthCircumference = 40075000L;
+        long globalGDP = 84000000000000L;
+        long speedOfLight = 299792458L;
+        long lifetimeOfSun = 10000000000L;
+        long distanceToNearestStar = 40208000000000L;
+
+        //Examples of float Datatype
+        float pi = 3.14159f;
+        float temperature = 25.5f;
+        float employeeSalary = 50000.0f;
+        float productPrice = 99.99f;
+        float distanceInKm = 1234.56f;
+        float weightInKg = 75.3f;
+        float interestRate = 0.05f;
+        float lengthInMeters = 10.0f;
+        float areaInSquareMeters = 100.0f;
+        float speedInMph = 65.5f;
+
+        //Examples of Double Datatype
+        double pid = 3.14159d;
+        double temperatured = 25.5d;
+        double employeeSalaryd = 50000.0d;
+        double productPriced = 99.99d;
+        double distanceInKmd = 1234.56d;
+        double weightInKgd = 75.3d;
+        double interestRated = 0.05d;
+        double lengthInMetersd = 10.0d;
+        double areaInSquareMetersd = 100.0d;
+        double speedInMphd = 65.5d;
+
+        //Examples of Char Datatype
+        char grade = 'A';
+        char firstLetter = 'J';
+        char symbol = '@';
+        char gender = 'M';
+        char punctuation = '.';
+        char vowel = 'E';
+        char currencySymbol = '$';
+        char digit = '5';
+        char direction = 'N';
+        char escapeSequence = '\n';
+
+        boolean isRaining = true;
+        boolean isSunny = false;
+        boolean hasPassedExam = true;
+        boolean isEmployeeActive = false;
+        boolean isLoggedIn = true;
+        boolean isOverdue = false;
+        boolean isFound = true;
+        boolean isComplete = false;
+        boolean isApproved = true;
+        boolean isLocked = false;
+
+    }
+}
